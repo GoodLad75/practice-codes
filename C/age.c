c93f8ba76695013b659541128809a0a83988bd38
lewispaolo <116490573+lewispaolo@users.noreply.github.com>
2022-11-15 22:20:27 +0800

    upload code

---
+++ C/age.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if(age >= 21){
+        printf("you can buy beer!");
+    }
+    else if (age < 10){
+        printf("why not buy some milk?");
+    }
+    else if (age == 15){
+        printf("have you tried vaping?");
+    }
+    else{
+        printf("go home!");
+    }
+
+    return 0;
+}
