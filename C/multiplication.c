1369c61230a9c8ed9fd00562ce9e66447b3957b6
lewispaolo <116490573+lewispaolo@users.noreply.github.com>
2022-11-15 22:53:01 +0800

    Create multiplication.c

---
+++ C/multiplication.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i,userInput,multiple;
+  
+  printf("Enter a number 1-10   ");
+  scanf("%d", &userInput);
+  i = userInput;
+  multiple = userInput;
+  
+  for (i; i <= 100; i+=multiple) {
+    printf("%d\n", i);
+  }
+  
+  return 0;
+}
